9bcb9098c9fd3ab78e79cab3644089f9d610dc5b
perryvv <perryvv@zeelandnet.nl>
2021-08-13 11:49:40 +0200

    Bootstrap toegevoegd

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+
+    #[Route('/', name: 'admin_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('admin/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'admin_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
